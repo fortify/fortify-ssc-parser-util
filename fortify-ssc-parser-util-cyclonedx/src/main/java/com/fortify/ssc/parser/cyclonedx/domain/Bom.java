2c09715e1ef659435624c95e35ae6efc9b6ecc43
Sangamesh Vijaykumar <svijaykumar@opentext.com>
2026-07-17 16:52:57 +0530

    feat: replace mapdb with in-memory HashMap, upgrade dependencies

--- fortify-ssc-parser-util-cyclonedx/src/main/java/com/fortify/ssc/parser/cyclonedx/domain/Bom.java
+++ fortify-ssc-parser-util-cyclonedx/src/main/java/com/fortify/ssc/parser/cyclonedx/domain/Bom.java
@@ -28,13 +28,12 @@ import java.io.IOException;
 import java.io.Serializable;
 import java.util.Arrays;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.stream.Collectors;
 
 import org.apache.commons.lang3.ArrayUtils;
 import org.apache.commons.lang3.StringUtils;
-import org.mapdb.DB;
-import org.mapdb.Serializer;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
@@ -46,97 +45,109 @@ import lombok.Getter;
 import lombok.Setter;
 
 public final class Bom implements Serializable {
-	private static final long serialVersionUID = 1L;
-	@Getter @Setter private BomFormat bomFormat;
-	@Getter @Setter private String specVersion;	
-	// private String serialNumber;
-	// private Integer version;
-	@Getter @Setter private BomMetadata metadata;
-	private final Map<String, Component> componentsByBomRef;
-	// private Service[] services;
-	// private ExternalReference[] externalReferences;
-	// private Dependency[] dependencies;
-	// private Composition[] compositions;
-	@Getter private Region vulnerabilitiesRegion = null;
-	//@JsonProperty private JSFSignature[] signature;
-	
-	public static enum BomFormat {
-		CycloneDX
-	}
-	
-	/**
-	 * Private constructor; instances can be created through the {@link #parseRunData(DB, ExtendedJsonParser)}
-	 * method.
-	 * 
-	 * @param db
-	 */
-	@SuppressWarnings("unchecked")
-    private Bom(final DB db) {
-		// We assume large scans may include a lot of components, so we use disk-backed collections.
-		// Note that alternatively we could use a hash & position-based approach like the SARIF .NET SDK
-		// (see DeferredDictionary and DeferredList) to avoid serializing entries to disk, but for now
-		// disk-backed collections seem to perform well and the implementation is much easier to understand.
-		this.componentsByBomRef = db.hashMap("componentsById", Serializer.STRING, Serializer.JAVA).create();
-	}
+    private static final long serialVersionUID = 1L;
+    @Getter
+    @Setter
+    private BomFormat bomFormat;
+    @Getter
+    @Setter
+    private String specVersion;
+    // private String serialNumber;
+    // private Integer version;
+    @Getter
+    @Setter
+    private BomMetadata metadata;
+    private final Map<String, Component> componentsByBomRef;
+    // private Service[] services;
+    // private ExternalReference[] externalReferences;
+    // private Dependency[] dependencies;
+    // private Composition[] compositions;
+    @Getter
+    private Region vulnerabilitiesRegion = null;
+    // @JsonProperty private JSFSignature[] signature;
 
-	public static final Bom parseBom(DB db, ExtendedJsonParser jsonParser) throws IOException {
-		Bom bom = new Bom(db);
-		new StreamingJsonParser()
-			.handler("/bomFormat", BomFormat.class, bom::setBomFormat)
-			.handler("/specVersion", String.class, bom::setSpecVersion)
-			.handler("/metadata", BomMetadata.class, bom::setMetadata)
-			.handler("/components/*", Component.class, bom::addComponent)
-			.handler("/vulnerabilities", bom::setVulnerabilitiesRegion)
-			.parseObjectProperties(jsonParser, "/");
-		return bom;
-	}
-	
-	public final Component getComponentByBomRef(String bomRef) {
-		return componentsByBomRef.get(bomRef);
-	}
-	
-	private final void addComponent(Component component) {
-		componentsByBomRef.put(component.getBomRef(), component);
-	}
-	
-	private final void setVulnerabilitiesRegion(ExtendedJsonParser jp) throws IOException {
-		this.vulnerabilitiesRegion = jp.getObjectOrArrayRegion();
-	}
-	
-	@Getter
-	public static final class BomMetadata {
-		@JsonProperty private Date timestamp;
-		@JsonProperty 
-		@JsonDeserialize(using = BomToolsDeserializer.class)
-		private BomTool[] tools;
-		//@JsonProperty private BomAuthor[] authors;
-		//@JsonProperty private Component component;
-		//@JsonProperty private BomManufacturer manufacture;
-		//@JsonProperty private BomSupplier manufacture;
-		//@JsonProperty private BomLicense[] licenses;
-		//@JsonProperty private Property[] properties;
-	}
-	
-	@Getter
-	public static final class BomTool {
-		@JsonProperty private String vendor;
-		@JsonProperty private String name;
-		@JsonProperty private String version;
-		//@JsonProperty private Hash[] hashes;
-		//@JsonProperty private ExternalReference[] externalReferences;
-	}
-	
-	public final String getToolName() {
-		String toolName = "Unknown";
-		if ( getMetadata()!=null && ArrayUtils.isNotEmpty(getMetadata().getTools()) ) {
-			BomTool mainTool = getMetadata().getTools()[0];
-			toolName = 
-					StringUtils.defaultIfBlank(mainTool.getVendor()+" ", "") 
-					+ StringUtils.defaultIfBlank(mainTool.getName()+" ", "")
-					+ StringUtils.defaultIfBlank(mainTool.getVersion()+" ", "");
-			// Remove duplicate words, for example if tool name repeats vendor name 
-			toolName = Arrays.stream( toolName.split("\\s+")).distinct().collect(Collectors.joining(" ") );;
-		}
-		return toolName;
-	}
+    public static enum BomFormat {
+        CycloneDX
+    }
+
+    /**
+     * Private constructor; instances can be created through the
+     * {@link #parseBom(ExtendedJsonParser)}
+     * method.
+     */
+    private Bom() {
+        // Use in-memory HashMap for storing components. This is more efficient than
+        // disk-backed
+        // MapDB storage as it reduces memory overhead and complexity. The component map
+        // is
+        // only used during parsing and vulnerability processing, not persisted beyond
+        // the
+        // current scan operation.
+        this.componentsByBomRef = new HashMap<>();
+    }
+
+    public static final Bom parseBom(ExtendedJsonParser jsonParser) throws IOException {
+        Bom bom = new Bom();
+        new StreamingJsonParser()
+                .handler("/bomFormat", BomFormat.class, bom::setBomFormat)
+                .handler("/specVersion", String.class, bom::setSpecVersion)
+                .handler("/metadata", BomMetadata.class, bom::setMetadata)
+                .handler("/components/*", Component.class, bom::addComponent)
+                .handler("/vulnerabilities", bom::setVulnerabilitiesRegion)
+                .parseObjectProperties(jsonParser, "/");
+        return bom;
+    }
+
+    public final Component getComponentByBomRef(String bomRef) {
+        return componentsByBomRef.get(bomRef);
+    }
+
+    private final void addComponent(Component component) {
+        componentsByBomRef.put(component.getBomRef(), component);
+    }
+
+    private final void setVulnerabilitiesRegion(ExtendedJsonParser jp) throws IOException {
+        this.vulnerabilitiesRegion = jp.getObjectOrArrayRegion();
+    }
+
+    @Getter
+    public static final class BomMetadata {
+        @JsonProperty
+        private Date timestamp;
+        @JsonProperty
+        @JsonDeserialize(using = BomToolsDeserializer.class)
+        private BomTool[] tools;
+        // @JsonProperty private BomAuthor[] authors;
+        // @JsonProperty private Component component;
+        // @JsonProperty private BomManufacturer manufacture;
+        // @JsonProperty private BomSupplier manufacture;
+        // @JsonProperty private BomLicense[] licenses;
+        // @JsonProperty private Property[] properties;
+    }
+
+    @Getter
+    public static final class BomTool {
+        @JsonProperty
+        private String vendor;
+        @JsonProperty
+        private String name;
+        @JsonProperty
+        private String version;
+        // @JsonProperty private Hash[] hashes;
+        // @JsonProperty private ExternalReference[] externalReferences;
+    }
+
+    public final String getToolName() {
+        String toolName = "Unknown";
+        if (getMetadata() != null && ArrayUtils.isNotEmpty(getMetadata().getTools())) {
+            BomTool mainTool = getMetadata().getTools()[0];
+            toolName = StringUtils.defaultIfBlank(mainTool.getVendor() + " ", "")
+                    + StringUtils.defaultIfBlank(mainTool.getName() + " ", "")
+                    + StringUtils.defaultIfBlank(mainTool.getVersion() + " ", "");
+            // Remove duplicate words, for example if tool name repeats vendor name
+            toolName = Arrays.stream(toolName.split("\\s+")).distinct().collect(Collectors.joining(" "));
+            ;
+        }
+        return toolName;
+    }
 }
--- fortify-ssc-parser-util-cyclonedx/src/main/java/com/fortify/ssc/parser/cyclonedx/parser/VulnerabilitiesParser.java
+++ fortify-ssc-parser-util-cyclonedx/src/main/java/com/fortify/ssc/parser/cyclonedx/parser/VulnerabilitiesParser.java
@@ -2,9 +2,6 @@ package com.fortify.ssc.parser.cyclonedx.parser;
 
 import java.io.IOException;
 
-import org.mapdb.DB;
-import org.mapdb.DBMaker;
-
 import com.fasterxml.jackson.core.JsonToken;
 import com.fortify.plugin.api.ScanData;
 import com.fortify.plugin.api.ScanEntry;
@@ -17,66 +14,70 @@ import com.fortify.util.json.ExtendedJsonParser;
 import com.fortify.util.ssc.parser.json.ScanDataStreamingJsonParser;
 
 /**
- * This class parses a CycloneDX JSON input document to generate Fortify vulnerabilities.
+ * This class parses a CycloneDX JSON input document to generate Fortify
+ * vulnerabilities.
  * 
  * @author Ruud Senden
  */
 public final class VulnerabilitiesParser {
-	private final ScanData scanData;
-	private final ScanEntry scanEntry;
-	private final VulnerabilitiesProducer vulnerabilitiesProducer;
-	
-	/**
-	 * Constructor for storing {@link ScanData} and {@link VulnerabilityHandler}
-	 * instances.
-	 * @param scanData
-	 * @param scanEntry 
-	 * @param vulnerabilityHandler
-	 */
-	public VulnerabilitiesParser(final ScanData scanData, ScanEntry scanEntry, final VulnerabilityHandler vulnerabilityHandler) {
-		this.scanData = scanData;
-		this.scanEntry = scanEntry;
-		this.vulnerabilitiesProducer = new VulnerabilitiesProducer(vulnerabilityHandler);
-	}
-	
-	public final void parse() throws ScanParsingException, IOException {
-		new ScanDataStreamingJsonParser()
-			.handler("/", this::parseBom)
-			.parse(scanData, scanEntry);
-	}
-	
-	/**
-	 * Main method to commence parsing the CycloneDX document provided by the
-	 * configured {@link ScanData}.
-	 * @throws IOException
-	 */
-	public final void parseBom(ExtendedJsonParser jsonParser) throws IOException {
-		try ( DB db = DBMaker.tempFileDB()
-				.closeOnJvmShutdown().fileDeleteAfterClose()
-				.fileMmapEnableIfSupported()
-				.make() ) {
-			Bom bom = Bom.parseBom(db, jsonParser);
-			parseVulnerabilities(bom);
-		}
-	}
+    private final ScanData scanData;
+    private final ScanEntry scanEntry;
+    private final VulnerabilitiesProducer vulnerabilitiesProducer;
 
-	/**
-	 * This method re-parses the CycloneDX <code>vulnerabilities</code> array, based on the
-	 * input document {@link Region} previously collected in the given {@link Bom}
-	 * object. For each entry in the <code>vulnerabilities</code> array:
-	 * <ol>
-	 *  <li>The JSON contents are mapped to a {@link Vulnerability} object</li>
-	 *  <li>The {@link Vulnerability} and {@link Bom} objects are passed to the
-	 *      {@link VulnerabilitiesProducer#produceVulnerability(Bom, Vulnerability)} method to produce
-	 *      the actual Fortify vulnerability (if applicable)</li>
-	 * </ol>
-	 * @param runData
-	 * @throws IOException
-	 */
-	private final void parseVulnerabilities(final Bom bom) throws IOException {
-		new ScanDataStreamingJsonParser()
-			.expectedStartTokens(JsonToken.START_ARRAY)
-			.handler("/*", Vulnerability.class, vuln->vulnerabilitiesProducer.produceVulnerabilities(bom, vuln))
-			.parse(scanData, scanEntry, bom.getVulnerabilitiesRegion());
-	}
+    /**
+     * Constructor for storing {@link ScanData} and {@link VulnerabilityHandler}
+     * instances.
+     * 
+     * @param scanData
+     * @param scanEntry
+     * @param vulnerabilityHandler
+     */
+    public VulnerabilitiesParser(final ScanData scanData, ScanEntry scanEntry,
+            final VulnerabilityHandler vulnerabilityHandler) {
+        this.scanData = scanData;
+        this.scanEntry = scanEntry;
+        this.vulnerabilitiesProducer = new VulnerabilitiesProducer(vulnerabilityHandler);
+    }
+
+    public final void parse() throws ScanParsingException, IOException {
+        new ScanDataStreamingJsonParser()
+                .handler("/", this::parseBom)
+                .parse(scanData, scanEntry);
+    }
+
+    /**
+     * Main method to commence parsing the CycloneDX document provided by the
+     * configured {@link ScanData}.
+     * 
+     * @throws IOException
+     */
+    public final void parseBom(ExtendedJsonParser jsonParser) throws IOException {
+        Bom bom = Bom.parseBom(jsonParser);
+        parseVulnerabilities(bom);
+    }
+
+    /**
+     * This method re-parses the CycloneDX <code>vulnerabilities</code> array, based
+     * on the
+     * input document {@link Region} previously collected in the given {@link Bom}
+     * object. For each entry in the <code>vulnerabilities</code> array:
+     * <ol>
+     * <li>The JSON contents are mapped to a {@link Vulnerability} object</li>
+     * <li>The {@link Vulnerability} and {@link Bom} objects are passed to the
+     * {@link VulnerabilitiesProducer#produceVulnerability(Bom, Vulnerability)}
+     * method to produce
+     * the actual Fortify vulnerability (if applicable)</li>
+     * </ol>
+     * 
+     * @param bom The {@link Bom} object containing the {@link Region} of the
+     *            <code>vulnerabilities</code> array in the input document
+     * @throws ScanParsingException
+     * @throws IOException
+     */
+    private final void parseVulnerabilities(final Bom bom) throws IOException {
+        new ScanDataStreamingJsonParser()
+                .expectedStartTokens(JsonToken.START_ARRAY)
+                .handler("/*", Vulnerability.class, vuln -> vulnerabilitiesProducer.produceVulnerabilities(bom, vuln))
+                .parse(scanData, scanEntry, bom.getVulnerabilitiesRegion());
+    }
 }
